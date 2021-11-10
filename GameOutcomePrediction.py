70442aa9d208c32d3c679326181e1867f78fa057
talhavawda <talhavawda.dev@gmail.com>
2021-11-10 21:23:54 +0200

    Testing out all 3 models and created additional test games
    - Selected on using the MLP Regression model

--- GameOutcomePrediction.py
+++ GameOutcomePrediction.py
@@ -14,7 +14,27 @@ def main():
 	print("NBA GAME OUTCOME PREDICTION")
 	print("============================")
 
-	model = load("MLPRegressor.joblib")
+	mlpRegressorModel = load("MLPRegressor.joblib")
+	svmRegressorModel = load("SVR.joblib")
+	linearRegressorModel = load("LinearRegression.joblib")
+
+	"""
+		Even though the Linear Regression model had the lowest error rates when training,
+		upon testing with test data  we have observed that 
+		it is giving incorrect output values - e.g. for GameOutcomeTest1.csv, the prediction
+		values are -1075.5001002610104 and -1180.663224851488, which are not probabilities, for
+		GameOutcomeTest2.csv the prediction values are: -1075.5001002610104 and -229.2144936475864,
+		and for GameOutcomeTest2.csv the prediction values are: 93.45959189775459 and -229.2144936475864
+		
+		Upon testing the SVM Regression model on our 3 test files, we found that for each time it was predicting
+		a win probability of 5052607385733374. 
+		
+		Thus we cannot use the Linear Regression and SVM Regression models in our final model, although we were considering
+		creating an ensemble model.
+		
+		Thus our final model is the MLP Regression model
+	"""
+
 
 	filePath = input("\nEnter the name of the textfile (located in the current directory) containing the feature vectors of the 2 teams: ")
 	# GameOutcomeTest1.csv
@@ -35,15 +55,31 @@ def main():
 	print()
 	print("Team 2's feature vector:")
 	print(featureVectorsDF.iloc[1].values.tolist())
+	print()
 
 	# Do the win probability predictions
-	winProbabilityPredictions = model.predict(featureVectorsDF)
+	print("Model - MLP Regression")
+	winProbabilityPredictions = mlpRegressorModel.predict(featureVectorsDF)
 	winProbabilityTeam1 = winProbabilityPredictions[0]
 	winProbabilityTeam2 = winProbabilityPredictions[1]
 
+
+	print("Win Probability of Team 1: ", winProbabilityTeam1)
+	print("Win Probability of Team 2: ", winProbabilityTeam2)
+
+	#winProbabilityPredictions = svmRegressorModel.predict(featureVectorsDF)
+	#winProbabilityTeam1 = winProbabilityPredictions[0]
+	#winProbabilityTeam2 = winProbabilityPredictions[1]
+	#print(winProbabilityTeam1, winProbabilityTeam2)
+
+	#winProbabilityPredictions = linearRegressorModel.predict(featureVectorsDF)
+	#winProbabilityTeam1 = winProbabilityPredictions[0]
+	#winProbabilityTeam2 = winProbabilityPredictions[1]
+	#print(winProbabilityTeam1, winProbabilityTeam2)
+
 	"""
 		Since the win probability prediction for each team is for that team in general only, normalise the probabilities
-		such that its relative to the other team and the sum of the 2 win probabilities add up till 1 
+		such that its relative to the other team,  and also to ensure that the sum of the 2 win probabilities add up to 1 
 	"""
 	normalisedWinProbabilityTeam1 = winProbabilityTeam1 / (winProbabilityTeam1 + winProbabilityTeam2)
 	normalisedWinProbabilityTeam2 = winProbabilityTeam2 / (winProbabilityTeam1 + winProbabilityTeam2)
